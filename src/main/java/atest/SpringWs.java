0aeb803d942d815dbcccdfc76dc2b09560ebba28
hanyong <313982441@qq.com>
2017-02-02 15:29:47 +0800

    SpringWs 取消延时发送消息

--- src/main/java/atest/SpringWs.java
+++ src/main/java/atest/SpringWs.java
@@ -9,7 +9,6 @@ public class SpringWs extends AbstractWebSocketHandler {
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
 		super.afterConnectionEstablished(session);
-		Thread.sleep(500);
 		session.sendMessage(new TextMessage("Hello WebSocket"));
 	}
 	
