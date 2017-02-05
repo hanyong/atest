1c67ce9429737a57ea311171ffb85b185c27604b
hanyong <313982441@qq.com>
2017-02-05 12:54:34 +0800

    SpringWs 修改发送 ping 消息, 打印 pong 消息

--- src/main/java/atest/SpringWs.java
+++ src/main/java/atest/SpringWs.java
@@ -1,5 +1,10 @@
 package atest;
 
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+
+import org.springframework.web.socket.PingMessage;
+import org.springframework.web.socket.PongMessage;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.AbstractWebSocketHandler;
@@ -9,7 +14,15 @@ public class SpringWs extends AbstractWebSocketHandler {
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
 		super.afterConnectionEstablished(session);
-		session.sendMessage(new TextMessage("Hello WebSocket"));
+//		session.sendMessage(new TextMessage("Hello WebSocket"));
+		ByteBuffer payload = ByteBuffer.wrap("hello".getBytes(StandardCharsets.UTF_8));
+		session.sendMessage(new PingMessage(payload));
+	}
+
+	@Override
+	protected void handlePongMessage(WebSocketSession session, PongMessage message) throws Exception {
+		String msg = new String(message.getPayload().array(), StandardCharsets.UTF_8);
+		System.out.println("spring get PongMessage: " + msg);
 	}
 	
 	@Override
